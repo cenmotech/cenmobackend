39650d169c5fbb3e7e5dabab9f86107b3aa8e8a7
Fikri Aufaa Zain <fikri.aufaa@ui.ac.id>
2023-03-02 23:19:25 +0700

    [RED] membust test_url_is_resolved_login

---
+++ authuser/tests/test_urls.py
@@ -0,0 +1,13 @@
+from django.urls import reverse, resolve
+from django.test import TestCase, RequestFactory
+from django.test import SimpleTestCase, TestCase, RequestFactory
+from authuser.views import register, login, logout
+
+# Create your tests here.
+class TestsUrls(TestCase): 
+    def test_url_is_resolved_login(self):
+        url = reverse('login')
+        self.assertEquals(resolve(url).func, login)
+
+
+
